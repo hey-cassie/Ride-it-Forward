cea84b386aac92dce1fd05c78c9080b781f9ce9e
Cassie Jones <cassie@Cassies-MacBook-Pro.local>
2021-03-13 13:30:47 -0600

    first commit

---
+++ ride-it-forward/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DonationFormComponent } from './donation-form/donation-form.component';
+
+import { ProfileComponent } from './profile/profile.component';
+import { StatsComponent } from './stats/stats.component';
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/profile', pathMatch: 'full' },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'stats', component: StatsComponent},
+    { path: 'donate', component: DonationFormComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
---
+++ ride-it-forward/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { StatsComponent } from './stats/stats.component';
+import { DonationFormComponent } from './donation-form/donation-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StatsComponent,
+    DonationFormComponent,
+    NavbarComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
